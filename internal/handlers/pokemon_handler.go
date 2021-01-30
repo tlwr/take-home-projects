9e7c548aee241f650cfa349163ccbf794899a331
toby lorne <toby@toby.codes>
2021-01-30 22:06:27 +0000

    pkg: add handlers package
    this hooks up an http server to the client
    
    Signed-off-by: toby lorne <toby@toby.codes>

---
+++ internal/handlers/pokemon_handler.go
@@ -0,0 +1,67 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"regexp"
+	"strings"
+
+	"github.com/tlwr/truelayer-take-home-pokemon-api/internal/pokemon"
+)
+
+var (
+	pokemonNameValidator = regexp.MustCompile("^[a-z]+$")
+)
+
+type PokemonHandler struct {
+	pc pokemon.PokemonClient
+}
+
+type GetPokemonResponse struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+}
+
+type GetPokemonErrorResponse struct {
+	Message string `json:"message"`
+}
+
+func writeJSON(rw http.ResponseWriter, status int, o interface{}) {
+	if b, err := json.Marshal(o); err != nil {
+		rw.WriteHeader(500)
+		rw.Write([]byte("could not marshal json"))
+	} else {
+		rw.Header().Set("Content-Type", "application/json")
+		rw.WriteHeader(status)
+		rw.Write(b)
+	}
+}
+
+func (h *PokemonHandler) HandleGet(rw http.ResponseWriter, req *http.Request) {
+	pokemonName := strings.TrimPrefix(req.URL.Path, "/pokemon/")
+
+	fmt.Println(pokemonName)
+
+	if !pokemonNameValidator.MatchString(pokemonName) {
+		writeJSON(rw, 400, GetPokemonErrorResponse{Message: "could not validate pokemon name"})
+		return
+	}
+
+	resp, err := h.pc.Get(strings.ToLower(pokemonName))
+	if err != nil {
+		writeJSON(rw, 500, GetPokemonErrorResponse{Message: fmt.Sprintf("%s", err)})
+		return
+	}
+
+	writeJSON(rw, 200, GetPokemonResponse{
+		Name:        resp.Name,
+		Description: resp.Description,
+	})
+}
+
+func NewPokemonHandler(pc pokemon.PokemonClient) *PokemonHandler {
+	return &PokemonHandler{
+		pc: pc,
+	}
+}
---
+++ internal/handlers/pokemon_handler_test.go
@@ -0,0 +1,85 @@
+package handlers_test
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	pokefake "github.com/tlwr/truelayer-take-home-pokemon-api/internal/fake_pokemon"
+	"github.com/tlwr/truelayer-take-home-pokemon-api/internal/handlers"
+	"github.com/tlwr/truelayer-take-home-pokemon-api/internal/pokemon"
+)
+
+var _ = Describe("PokemonHandler", func() {
+	var (
+		req      *http.Request
+		recorder *httptest.ResponseRecorder
+
+		h  *handlers.PokemonHandler
+		pc *pokefake.FakePokemonClient
+	)
+
+	BeforeEach(func() {
+		req = &http.Request{}
+		recorder = httptest.NewRecorder()
+
+		pc = &pokefake.FakePokemonClient{}
+		h = handlers.NewPokemonHandler(pc)
+	})
+
+	Describe("Get", func() {
+		Context("happy path", func() {
+			BeforeEach(func() {
+				pc.GetReturns(pokemon.GetPokemonResponse{
+					Name:        "pikachu",
+					Description: "a sparky boi",
+				}, nil)
+
+				req.URL = &(url.URL{Path: "/pokemon/pikachu"})
+			})
+
+			It("returns 200 with name and description", func() {
+				h.HandleGet(recorder, req)
+
+				resp := recorder.Result()
+
+				Expect(resp).NotTo(BeNil())
+				Expect(resp.StatusCode).To(Equal(200))
+				Expect(resp.Header.Get("Content-Type")).To(Equal("application/json"))
+
+				b, err := ioutil.ReadAll(resp.Body)
+				Expect(err).NotTo(HaveOccurred())
+
+				Expect(b).To(MatchJSON(`{"name": "pikachu", "description": "a sparky boi"}`))
+			})
+		})
+
+		Context("when the pokemon client returns an error", func() {
+			BeforeEach(func() {
+				pc.GetReturns(pokemon.GetPokemonResponse{}, fmt.Errorf("oh no"))
+
+				req.URL = &(url.URL{Path: "/pokemon/pikachu"})
+			})
+
+			It("returns 500 with name and description", func() {
+				h.HandleGet(recorder, req)
+
+				resp := recorder.Result()
+
+				Expect(resp).NotTo(BeNil())
+				Expect(resp.StatusCode).To(Equal(500))
+				Expect(resp.Header.Get("Content-Type")).To(Equal("application/json"))
+
+				b, err := ioutil.ReadAll(resp.Body)
+				Expect(err).NotTo(HaveOccurred())
+
+				Expect(b).To(MatchJSON(`{"message": "oh no"}`))
+			})
+		})
+	})
+})
