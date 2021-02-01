f47c63ecb973b3fbc3ef6e99e7c5e1e818dcb5cd
toby lorne <toby@toby.codes>
2021-02-01 21:07:46 +0000

    main: integrate all our packages
    this works for a happy path, and reports all the URLs
    
    however there are some ergonomic issues to resolve, eg host CLI args,
    HTTP status codes
    
    Signed-off-by: toby lorne <toby@toby.codes>

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/tlwr/monzo-take-home-crawler
+
+go 1.16
+
+require (
+	github.com/antchfx/htmlquery v1.2.3
+	github.com/goware/urlx v0.3.1
+	github.com/jarcoal/httpmock v1.0.8
+	github.com/kkdai/bloomfilter v0.0.0-20181031180009-70a84758d134
+	github.com/onsi/ginkgo v1.14.2
+	github.com/onsi/gomega v1.10.4
+	github.com/tlwr/operator-tools v0.0.0-20210119154217-648960b306ac
+	golang.org/x/net v0.0.0-20201202161906-c7110b5ffcbb
+)
---
+++ main.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"regexp"
+	"strings"
+	"sync"
+
+	"github.com/goware/urlx" // net/url url.Parse is not great for strict URL parsing
+
+	fllag "github.com/tlwr/monzo-take-home-crawler/internal/flag"
+	queue "github.com/tlwr/monzo-take-home-crawler/internal/url_dedup_queue"
+	scraper "github.com/tlwr/monzo-take-home-crawler/internal/web_page_scraper"
+
+	cl "github.com/tlwr/operator-tools/pkg/colour"
+)
+
+type result struct {
+	url   *url.URL
+	links []*url.URL
+}
+
+func main() {
+	var (
+		u          string
+		hosts      fllag.StringSliceFlag
+		hostRegexp = regexp.MustCompile("(?i)^([a-z-0-9]+[.])+[a-z]{2,}$") // naive regexp does not support unicode URLs
+	)
+
+	flag.StringVar(&u, "url", "", "page on which to begin crawling")
+	flag.Var(&hosts, "host", "crawls pages from this host (valid multiple times)")
+	flag.Parse()
+
+	if len(hosts) == 0 {
+		log.Fatal("host flag is required")
+	}
+
+	if u == "" {
+		log.Fatal("url flag is required")
+	}
+
+	ur, err := urlx.Parse(u)
+	if err != nil {
+		log.Fatalf("could not validate url: %v", err)
+	}
+
+	for _, host := range hosts {
+		if !hostRegexp.MatchString(host) {
+			log.Fatalf(`host "%s" is not valid`, host)
+		}
+	}
+
+	log.Printf("will crawl %s", ur)
+	for _, host := range hosts {
+		log.Printf("will crawl %s", host)
+	}
+
+	errC := make(chan error, 8)
+	resultsC := make(chan result, 64)
+
+	go func() {
+		for err := range errC {
+			log.Println(cl.Red(fmt.Sprintf("scraper encountered error: %s", err)))
+		}
+	}()
+
+	resultsWg := sync.WaitGroup{}
+	resultsWg.Add(1)
+	go func() {
+		for r := range resultsC {
+			log.Printf("results for page: %s", cl.Green(r.url.String()))
+			for _, l := range r.links {
+				log.Println(cl.Blue(l.String()))
+			}
+		}
+		resultsWg.Done()
+	}()
+
+	q := queue.New()
+	s := scraper.New(http.DefaultClient)
+
+	log.Printf("queueing first url %s", ur)
+	q.Enqueue(ur)
+
+	numWorkers := 1
+	for wi := 0; wi <= numWorkers; wi++ {
+		go q.Iter(func(u *url.URL) {
+			res, err := s.Scrape(u)
+
+			if err != nil {
+				errC <- err
+				return
+			}
+
+			for _, err := range res.ParseErrors {
+				errC <- err
+			}
+
+			for _, link := range res.Links {
+				for _, h := range hosts {
+					// case insensitive host comparison
+					if strings.EqualFold(h, link.Host) {
+						q.Enqueue(link)
+						break
+					}
+				}
+			}
+
+			resultsC <- result{
+				url:   u,
+				links: res.Links,
+			}
+		})
+	}
+
+	q.Wait()
+
+	close(errC)
+	close(resultsC)
+
+	resultsWg.Wait()
+}
